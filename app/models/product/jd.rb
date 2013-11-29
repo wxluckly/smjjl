997429ce42b125ae633c53b2383dae7426c2f26a
Joey wang <wxluckly@gmail.com>
2013-11-29 22:57:00 +0800

    change the method getting price

--- app/models/product/jd.rb
+++ app/models/product/jd.rb
@@ -13,21 +13,20 @@ class Product::Jd < Product
   def get_content
     page = Nokogiri::HTML(http_get(url), nil, 'gbk')
     update( 
-      name: page.css("#name h1").text,
-      price_key: (page.css("#product-intro script").text.scan(/SkuId":(\d+)/).last.first rescue nil))
+      name: page.css("#name h1").text
+      #, price_key: (page.css("#product-intro script").text.scan(/SkuId":(\d+)/).last.first rescue nil)
+    )
     get_price
     sleep 0.01
   end
 
   def get_price
     return if self.price_key.nil?
-    # return if Time.now - updated_at < 1.hours
     page = Nokogiri::HTML(http_get("http://p.3.cn/prices/mgets?skuIds=J_#{self.price_key}"))
     if value = (page.text.scan(/p"\:"([\d\.]+)/).first.first rescue nil)
       update(low_price: value.to_i) if low_price.blank?
       record_bargain value
     end
-    # touch
   end
 
   def record_bargain value
