aa9b74b27d834740f4a799401a080090ef04906c
Joey wang <wxluckly@gmail.com>
2013-12-01 14:04:21 +0800

    use backfill option

--- app/workers/daemon_price_worker.rb
+++ app/workers/daemon_price_worker.rb
@@ -2,7 +2,10 @@ class DaemonPriceWorker
   include Sidekiq::Worker
   include Sidetiq::Schedulable
 
-  recurrence { daily.hour_of_day(2, 10, 18) }
+  # recurrence { daily.hour_of_day(2, 10, 18) }
+  recurrence backfill: true do
+    daily.hour_of_day(2, 10, 18)
+  end
 
   @queue = :daemon
 
