35470e7c814270c3080ba41c58b02eac4ed3adb2
Joey wang <wxluckly@gmail.com>
2013-12-09 17:10:10 +0800

    set schedule to restart worker every hour

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,16 +3,21 @@ set :output, "log/cron_log.log"
 set :job_template, "bash -l -c ':job'"
 
 # 抓取id
-every :day, :at => '1:00am' do
+every :day, :at => '1:00am', :roles => [:master] do
   rake "daemon:get_id"
 end
 
 # 更新内容
-every :day, :at => '4:00am' do
+every :day, :at => '4:00am', :roles => [:master] do
   rake "daemon:update_content"
 end
 
 # 抓取价格
-every '0 8,14,20 * * *' do
+every '0 8,14,20 * * *', :roles => [:master] do
   rake "daemon:update_price"
+end
+
+# 每小时重启worker服务
+every '10 * * * *', :roles => [:worker] do
+  rake "sidekiq:restart"
 end
