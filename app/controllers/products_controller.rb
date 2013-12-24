2f37488f0d1bccd1854d0f506748efa07b612b5d
Joey wang <wxluckly@gmail.com>
2013-12-24 22:19:56 +0800

    fix bug

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,6 +6,6 @@ class ProductsController < ApplicationController
     # f.title({ :text=>"价格走势"})
     # f.options[:xAxis][:categories] = prices_array.map(&:created_at)
     # f.series(:type=> 'spline',:name=> '价格', :data=> prices_array.map{|p|p.value.to_i})
-  end
+    # end
   end
 end
