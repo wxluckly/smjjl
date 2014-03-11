262ccb068c3a696de580b78a40bb430bca49a241
Joey wang <wxluckly@gmail.com>
2014-03-11 21:43:53 +0800

    reduce update price times

--- config/schedule.rb
+++ config/schedule.rb
@@ -8,12 +8,12 @@ every :day, :at => '1:00am', :roles => [:master] do
 end
 
 # 更新内容
-every :day, :at => '4:00am', :roles => [:master] do
+every :day, :at => '5:00am', :roles => [:master] do
   rake "daemon:update_content"
 end
 
 # 抓取价格
-every '0 8,14,20 * * *', :roles => [:master] do
+every '0 8,16 * * *', :roles => [:master] do
   rake "daemon:update_price"
 end
 
