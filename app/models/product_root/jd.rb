caabcd400260df40f81ccce56e61b26a1db33b00
Joey wang <wxluckly@gmail.com>
2015-10-29 14:40:59 +0800

    coding

--- app/models/product_root/jd.rb
+++ app/models/product_root/jd.rb
@@ -10,7 +10,7 @@ class ProductRoot::Jd < ProductRoot
   # class methods .............................................................
   # public instance methods ...................................................
   def get_lists
-    Nokogiri::HTML(http_get(url), nil, Site::Jd::ENCODING).css("#sortlist .item a").map{ |a| a.attr("href") }.each do |url|
+    Nokogiri::HTML(http_get(url), nil, Site::Jd::ENCODING).css('.crumbs-nav-item').last.css('a').map{ |a| a.attr("href") }.each do |url|
       ProductList::Jd.create(url: url)
     end
   end
