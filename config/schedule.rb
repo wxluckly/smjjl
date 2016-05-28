fc0353bd7309782f5b6860213cf43b7355524c47
Joey Wang <wxluckly@gmail.com>
2016-05-28 23:56:16 +0800

    coding

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,17 +3,17 @@ set :output, "log/cron_log.log"
 set :job_template, "bash -l -c ':job'"
 
 # 抓取id
-every :day, :at => '1:00am', :roles => [:master] do
+every :day, :at => '2:00am', :roles => [:master] do
   rake "daemon:get_id"
 end
 
 # 更新内容
-every :day, :at => '3:00am', :roles => [:master] do
+every :day, :at => '4:00am', :roles => [:master] do
   rake "daemon:update_content"
 end
 
 # 抓取价格
-every '0 0,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23 * * *', :roles => [:master] do
+every '0 0,1,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23 * * *', :roles => [:master] do
   rake "daemon:update_price"
 end
 
