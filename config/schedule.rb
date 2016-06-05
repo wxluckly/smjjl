3d545c332e995fb8c0b2aaee7dab3ee863353896
Joey Wang <wxluckly@gmail.com>
2016-06-05 13:10:39 +0800

    清除失败任务，防止队列过满

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,6 +2,8 @@ env :PATH, ENV['PATH']
 set :output, "log/cron_log.log"
 set :job_template, "bash -l -c ':job'"
 
+# ------------------------ master ------------------------
+
 # 抓取id
 every :day, :at => '2:00am', :roles => [:master] do
   rake "daemon:get_id"
@@ -10,6 +12,11 @@ end
 # 更新内容
 every :day, :at => '4:00am', :roles => [:master] do
   rake "daemon:update_content"
+end
+
+# 清理失败的任务
+every '59 6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23 * * *', :roles => [:master] do
+  rake "daemon:clear_jobs"
 end
 
 # 抓取价格
@@ -21,6 +28,8 @@ end
 every '0 * * * *', :roles => [:master] do
   command "cd /www/smjjl && > log/production.log"
 end
+
+# ------------------------ worker ------------------------
 
 # 每小时零55分重启worker服务
 every '55 * * * *', :roles => [:worker] do
--- lib/tasks/daemon.rake
+++ lib/tasks/daemon.rake
@@ -16,4 +16,17 @@ namespace :daemon do
     ProductList.unblocked.priored.select(:id).each{ |l| GetPaginationWorker.perform_async(l.id, "price") }
   end
 
+  desc "清理失败的任务"
+  task :clear_jobs => :environment do
+    rs = Sidekiq::RetrySet.new
+    if rs.size > 0
+      rs.clear
+    end
+
+    update_list_price = Sidekiq::Queue.new("update_list_price")
+    if update_list_price.size > 0
+      update_list_price.clear
+    end
+  end
+
 end
