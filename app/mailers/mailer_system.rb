1603b7578001031f83800561ddcb1deb22b555f5
Joey Wang <wxluckly@gmail.com>
2016-06-08 00:28:25 +0800

    coding

--- app/mailers/mailer_system.rb
+++ app/mailers/mailer_system.rb
@@ -2,8 +2,8 @@ class MailerSystem < ActionMailer::Base
   default from: 'thinkgrand_poster@163.com'
 
   def crawler_notify
-    email = "44289889@qq.com"
-    mail(to: email, subject: '【什么降价了】爬虫通知')
+    emails = ["44289889@qq.com", "626382735@qq.com"]
+    mail(to: emails, subject: '【什么降价了】爬虫通知')
   end
 
 end
