a83f6fdba0fef715b7b4acd5db01b8476b7fc6b7
Joey Wang <wxluckly@gmail.com>
2016-06-15 00:09:01 +0800

    搜索功能

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -2,7 +2,11 @@ class IndexController < ApplicationController
   before_action :wx_auth!, only: [:test]
 
   def index
-    if params[:discount].present?
+    if (params[:search] || {})[:product_name_cont].present?
+      @bargains = Bargain.where('bargains.created_at < ?', Time.now - 10.days).search(params[:search]).result
+      @bargains = @bargains.where("discount >= '#{1 - params[:discount].to_f / 10 }'") if params[:discount].present?
+      @bargains = @bargains.order("created_at desc").includes(:product).paginate(page: params[:page], per_page: 50, total_entries: 50)
+    elsif params[:discount].present?
       @bargains = Bargain.where("discount >= '#{1 - params[:discount].to_f / 10 }'").order("created_at desc").includes(:product).paginate(page: params[:page], per_page: 50, total_entries: 500)
     else
       @bargains = Bargain.order("created_at desc").includes(:product).paginate(page: params[:page], per_page: 50, total_entries: 500)
