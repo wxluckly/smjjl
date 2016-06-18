4902f6f87b1a458250628580c9beb772c0bb1caf
Joey Wang <wxluckly@gmail.com>
2016-06-18 15:57:55 +0800

    coding

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -7,7 +7,12 @@ class IndexController < ApplicationController
       @bargains = @bargains.where("discount >= '#{1 - params[:discount].to_f / 10 }'") if params[:discount].present?
       @bargains = @bargains.order("created_at desc").includes(:product).paginate(page: params[:page], per_page: 50, total_entries: 50)
     elsif params[:discount].present?
-      @bargains = Bargain.where("discount >= '#{1 - params[:discount].to_f / 10 }'").order("created_at desc").includes(:product).paginate(page: params[:page], per_page: 50, total_entries: 500)
+      if params[:category].present?
+        bargains_categories = BargainsCategory.where(category_id: params[:category]).includes([bargain: :product])
+        @bargains = bargains_categories.where("bargains.discount >= '#{1 - params[:discount].to_f / 10 }'").order("created_at desc").includes(:product).paginate(page: params[:page], per_page: 50, total_entries: 500)
+      else
+        @bargains = Bargain.where("discount >= '#{1 - params[:discount].to_f / 10 }'").order("created_at desc").includes(:product).paginate(page: params[:page], per_page: 50, total_entries: 500)
+      end
     else
       @bargains = Bargain.order("created_at desc").includes(:product).paginate(page: params[:page], per_page: 50, total_entries: 500)
     end
