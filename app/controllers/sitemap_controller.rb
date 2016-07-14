b73d349036f91e8bf841e2a217633ad8fd327019
Joey Wang <wxluckly@gmail.com>
2016-07-14 17:31:57 +0800

    coding

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -1,6 +1,6 @@
 class SitemapController < ApplicationController
   def index
     page = params[:page].to_i > 200 ? 200 : params[:page]
-    @products = Product.select("id, name, created_at").order("id desc").paginate(page: page, per_page: 50, total_entries: 10000 )
+    @products = Product.where.not(name: nil).select("id, name, created_at").order("id desc").paginate(page: page, per_page: 50, total_entries: 10000 )
   end
 end
