ca77c65b358b7ee0f956c8762af5a65048453bdc
Joey Wang <wxluckly@gmail.com>
2016-07-14 18:08:05 +0800

    coding

---
+++ app/workers/linksubmit_worker.rb
@@ -0,0 +1,15 @@
+class LinksubmitWorker
+  include Sidekiq::Worker
+  sidekiq_options :queue => :lintsubmit
+
+  def perform(product_id)
+    product = Product.find(product_id)
+    urls = ["http://www.smjjl.com/products/#{product.to_param}"]
+    uri = URI.parse('http://data.zz.baidu.com/urls?site=www.smjjl.com&token=NdXEXgoFUucOFtHn')
+    req = Net::HTTP::Post.new(uri.request_uri)
+    req.body = urls.join("\n")
+    req.content_type = 'text/plain'
+    res = Net::HTTP.start(uri.hostname, uri.port) { |http| http.request(req) }
+    puts res.body
+  end
+end
--- app/workers/update_content_worker.rb
+++ app/workers/update_content_worker.rb
@@ -4,5 +4,6 @@ class UpdateContentWorker
 
   def perform(product_id)
     Product.find(product_id).get_content
+    LinksubmitWorker.perform_async(product_id)
   end
 end
