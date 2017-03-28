fb6233c48f5f44f1138c335c6aad54144beac477
Joey Wang <wxluckly@gmail.com>
2017-03-28 23:28:18 +0800

    coding

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ set :bundle_roles, :all
 set :ssh_options, {user: 'www'}
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 'config/settings/config.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 'config/settings/config.yml', 'config/wechat.yml')
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
